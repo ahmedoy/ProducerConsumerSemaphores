f6f51f839afa79280b15e157b3b67a5a1b2c8366
ahmedoy <ahmedoy34@gmail.com>
2023-12-21 11:04:14 +0200

    Initial Commit

---
+++ semaphores.c
@@ -0,0 +1,23 @@
+#include <pthread.h>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <string.h>
+
+#include <semaphore.h>
+
+#define NUMBER_OF_COUNTER_THREADS 8
+
+
+
+
+
+
+int main(){
+
+    
+
+
+}
